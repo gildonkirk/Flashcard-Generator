f869207c773d738bbfd637de1e009131dfba9291
Gildon Kirk <gildonkirk@gmail.com>
2017-07-31 16:20:31 -0400

    set time out for answer to give user time to answer question, error message for cloze

--- basicCard.js
+++ basicCard.js
@@ -6,15 +6,20 @@ var BasicCard = function(front, back){
 var questions = [];
 var basicCardOne = new BasicCard('On which planet does Yoda train Luke?', 'Dagobah');
 var basicCardTwo = new BasicCard('What did Samuel L. Jackson request from George Lucas in order to be in the Star Wars prequels?', 'Purple Lightsaber');
+var basicCardThree = new BasicCard('What snow creature attacks Luke at the beginning of The Empire Strikes Back?', 'A Wampa');
+var basicCardFour = new BasicCard('Who said, "Aren\'t you a little short to be a Storm Trooper?"', 'Princess Leia');
+
 
 questions.push(basicCardOne);
 questions.push(basicCardTwo);
+questions.push(basicCardThree);
 
 var selection = 0;
 function randomQuestion() {
 	selection = Math.floor(Math.random() * (questions.length));
-	console.log(questions[selection].front)
-	console.log(questions[selection].back)
+	console.log('You have 10 Seconds to answer the following question:')
+	setTimeout(function(){console.log(questions[selection].front);}, 3000);
+	setTimeout(function(){console.log(questions[selection].back);}, 13000);
 }
 
 randomQuestion();
--- clozeCard.js
+++ clozeCard.js
@@ -7,16 +7,27 @@ var ClozeCard = function(text, cloze) {
 var questions = [];
 var clozeCardOne = new ClozeCard('Samuel L. Jackson requested from George Lucas that his lightsaber be purple.', 'his lightsaber be purple.');
 var clozeCardTwo = new ClozeCard('Dagobah is the planet on which Yoda trains Luke.', 'Dagobah');
+var clozeCardThree = new ClozeCard('A Wampa attacks Luke at the beginning of The Empire Strikes Back.', 'A Wampa');
+var clozeCardFour = new ClozeCard('Princess Leia said, "Aren\'t you a little short to be a Storm Trooper?".', 'Princess Leia');
+var clozeCardError = new ClozeCard('Throw', 'Error');
+
 
 questions.push(clozeCardOne);
 questions.push(clozeCardTwo);
+questions.push(clozeCardThree);
+questions.push(clozeCardError);
 
 var selection = 0;
 function randomQuestion() {
 	selection = Math.floor(Math.random() * (questions.length));
-	console.log(questions[selection].partial)
-	console.log(questions[selection].cloze)
-}
+	if (questions[selection].fullText.indexOf(questions[selection].cloze) !== -1){
+		console.log('You have 10 Seconds to say the missing word or phrase:')
+		setTimeout(function(){console.log(questions[selection].partial);}, 3000);
+		setTimeout(function(){console.log(questions[selection].cloze);}, 13000);
+	} else {
+		console.log('Error: cloze not part of full text.');
+	};
+};
 
 randomQuestion();
 
